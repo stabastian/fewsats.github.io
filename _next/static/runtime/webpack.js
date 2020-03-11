5d134b7e1857c929ac6d2be52228e3c9c2a2dfb2
fewsats <xxasasasax@xxxxxxxxxxx.xxx>
2020-03-11 11:49:41 +0100

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7750cee9ff56a97ca71b";
+/******/ 	var hotCurrentHash = "a5b82b743bc1e6abf434";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -311,7 +311,6 @@
 /******/ 			for(var chunkId in installedChunks)
 /******/ 			// eslint-disable-next-line no-lone-blocks
 /******/ 			{
-/******/ 				/*globals chunkId */
 /******/ 				hotEnsureUpdateChunk(chunkId);
 /******/ 			}
 /******/ 			if (
---
+++ _next/static/webpack/static/development/pages/compound-interest-calculator.js.ffd64b62894f8e79a590.hot-update.js
@@ -0,0 +1,142 @@
+webpackHotUpdate("static/development/pages/compound-interest-calculator.js",{
+
+/***/ "./components/calculator-row.js":
+/*!**************************************!*\
+  !*** ./components/calculator-row.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/extends */ "./node_modules/@babel/runtime/helpers/esm/extends.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var clsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! clsx */ "./node_modules/clsx/dist/clsx.m.js");
+/* harmony import */ var react_tooltip__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-tooltip */ "./node_modules/react-tooltip/dist/index.es.js");
+/* harmony import */ var react_icons_fi__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-icons/fi */ "./node_modules/react-icons/fi/index.esm.js");
+
+var _jsxFileName = "/Users/aarnod01/code/trading-tools/components/calculator-row.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+
+
+
+
+var CalculatorRow = function CalculatorRow(_ref) {
+  var label = _ref.label,
+      value = _ref.value,
+      name = _ref.name,
+      readOnly = _ref.readOnly,
+      onChange = _ref.onChange,
+      tip = _ref.tip,
+      variant = _ref.variant,
+      _ref$type = _ref.type,
+      type = _ref$type === void 0 ? 'number' : _ref$type;
+  return __jsx("tr", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, __jsx("td", {
+    className: Object(clsx__WEBPACK_IMPORTED_MODULE_2__["default"])(readOnly ? 'bg-background' : 'bg-muted font-semibold', variant === 'long' && 'text-green font-bold', variant === 'short' && 'text-red font-bold', !variant && 'text-text font-semi-bold', 'truncate p-1 w-2/3 border border-highlight'),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "flex items-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "mr-2 ml-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, __jsx("div", {
+    "data-tip": true,
+    "data-for": label,
+    className: "flex items-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, __jsx(react_icons_fi__WEBPACK_IMPORTED_MODULE_4__["FiHelpCircle"], {
+    className: "text-gray-300 transition-all hover:text-lgray inline-block h-5 w-5 cursor-pointer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  })), __jsx(react_tooltip__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    id: label,
+    event: "click focus",
+    className: "customTooltipTheme",
+    globalEventOff: "click",
+    effect: "solid",
+    place: 'right',
+    clickable: true,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "text-xs font-light p-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, tip)))), __jsx("div", {
+    className: "text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }, label))), __jsx("td", {
+    className: "p-1 w-1/3 border border-highlight",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 51
+    },
+    __self: this
+  }, __jsx("input", Object(_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    className: Object(clsx__WEBPACK_IMPORTED_MODULE_2__["default"])('text-sm w-full text-right bg-background', variant === 'long' && 'text-green font-bold', variant === 'short' && 'text-red font-bold', !variant && (readOnly ? 'text-lgray ' : 'text-text')),
+    type: type,
+    name: name,
+    onChange: onChange,
+    value: value,
+    readOnly: readOnly
+  }, readOnly && {
+    tabIndex: '-1'
+  }, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (CalculatorRow);
+
+/***/ })
+
+})
+//# sourceMappingURL=compound-interest-calculator.js.ffd64b62894f8e79a590.hot-update.js.map
